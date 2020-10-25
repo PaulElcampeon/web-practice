80ce47f8d00ec299f7cc407379a73f26b4d10958
PaulElcampeon <pauloladele@live.co.uk>
2020-10-25 15:16:55 +0000

    First commit.

---
+++ src/test/java/org/paulo/webdemo/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paulo.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
